37d643b54821686b97a610b64d3abab7f18dfef3
shiva-kannan <sssk2006@gmail.com>
2020-12-16 20:17:36 -0500

    Adding WIP editor tool

---
+++ Assets/Scripts/Editor/FMODNewSongSetupInspector.cs
@@ -0,0 +1,55 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+[CustomEditor(typeof(FMODNewSongSetup))]
+public class FMODNewSongSetupInspector : Editor
+{
+    public int selectedReverbTrack;
+    public bool useEventAsTrackName;
+        
+    public override void OnInspectorGUI () {
+        base.DrawDefaultInspector();
+
+        FMODNewSongSetup fmodNewSongSetup = (FMODNewSongSetup) target;
+        
+        GUILayout.Space(20f); //2
+        GUILayout.Label("Setup Options", EditorStyles.boldLabel); //3
+        
+        GUILayout.Space(10f);
+        selectedReverbTrack = EditorGUILayout.Popup("Pick Track for Reverb: ", selectedReverbTrack,
+            fmodNewSongSetup.songTackReferences.ToArray()); 
+
+        GUILayout.Space(10f);
+
+        useEventAsTrackName = GUILayout.Toggle(useEventAsTrackName, "  Use event name as track name");
+
+        GUILayout.Space(10f);
+
+        GUILayout.BeginHorizontal();
+        GUILayout.Space(20f);
+
+        if (GUILayout.Button($"Create Scenes for {fmodNewSongSetup.songName}"))
+        {
+            // Setup the new scenes with the available information
+        }
+        GUILayout.Space(20f);
+        GUILayout.EndHorizontal();
+        
+        GUILayout.Space(10);
+        GUILayout.Label("Build Settings", EditorStyles.boldLabel);
+        GUILayout.Space(5);
+        GUILayout.BeginHorizontal();
+        if (GUILayout.Button("Build for Quest"))
+        {
+            // Trigger build for Quest
+        }
+
+        if (GUILayout.Button("Build for Rift S"))
+        {
+            // Trigger build for Rift S
+        }
+        GUILayout.EndHorizontal();
+        
+    }
+
+}
---
+++ Assets/Scripts/FMODNewSongSetup.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FMODNewSongSetup : MonoBehaviour
+{
+    public string songName;
+    public int numberOfStems;
+    public int songBPM;
+    public int songBeatPerMeausure;
+    public int totalNumberofMeasures;
+    
+    [FMODUnity.EventRef] public List<string> songTackReferences;
+    
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
